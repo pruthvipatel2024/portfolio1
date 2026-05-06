3c66644f8537750be916ba3b1f60d6c4c07648c1
pruthvipatel2024 <patelpruthvi671@gmail.com>
2026-05-06 14:55:14 +0530

    speed insight is include

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import MouseFollower from './components/MouseFollower';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
   return (
