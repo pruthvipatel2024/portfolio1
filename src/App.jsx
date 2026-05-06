6ef5cfb5b487fb8a0234c34aff8dd7dc178c24c6
pruthvipatel2024 <patelpruthvi671@gmail.com>
2026-05-06 14:57:09 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import MouseFollower from './components/MouseFollower';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
